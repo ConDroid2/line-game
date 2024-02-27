278e2a11f1c6edc6c3fd5fc9b1b54330aea60d17
ConDroid2 <ronnoc918@gmail.com>
2024-02-27 14:21:27 -0600

    Made it a bit cleaner when lines edges touch

--- Assets/Code/LevelComponents/Lines/LineDrawer.cs
+++ Assets/Code/LevelComponents/Lines/LineDrawer.cs
@@ -9,24 +9,21 @@ public class LineDrawer : MonoBehaviour
     [SerializeField] private LineRenderer _renderer;
     [SerializeField] private LineController _controller;
 
+    private Vector3 _visualA;
+    private Vector3 _visualB;
+
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         if (_renderer != null)
         {
-            _renderer.SetPosition(0, _controller.InitialA);
-            _renderer.SetPosition(1, _controller.InitialB);
+            UpdateLinePositions();
         }
         else
         {
             Debug.LogWarning("No line renderer");
         }
-    }
-
-    private void Start()
-    {
-        UpdateLinePositions();
     }
 
     private void Update()
@@ -39,8 +36,10 @@ public class LineDrawer : MonoBehaviour
 
     private void UpdateLinePositions()
     {
-        _renderer.SetPosition(0, _controller.CurrentA);
-        _renderer.SetPosition(1, _controller.CurrentB);
+        SetVisualEndpoints();
+
+        _renderer.SetPosition(0, _visualA);
+        _renderer.SetPosition(1, _visualB);
     }
 
     // When passing in, 0 means opaque, 1 means transparent
@@ -50,5 +49,14 @@ public class LineDrawer : MonoBehaviour
 
         _renderer.startColor = new Color(_renderer.startColor.r, _renderer.startColor.g, _renderer.startColor.b, newAlpha);
         _renderer.endColor = new Color(_renderer.endColor.r, _renderer.endColor.g, _renderer.endColor.b, newAlpha);
+    }
+
+    private void SetVisualEndpoints()
+    {
+        Vector3 slope = _controller.Slope;
+        float halfWidth = _renderer.startWidth / 2;
+
+        _visualA = _controller.CurrentA - (slope.normalized * halfWidth);
+        _visualB = _controller.CurrentB + (slope.normalized * halfWidth);
     }
 }
