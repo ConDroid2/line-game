5d5b1ad3dc32d7aad43ea8fac3ca90f43cfdd91d
ConDroid2 <ronnoc918@gmail.com>
2024-06-04 23:10:34 -0500

    Added shader to allow the use of emission maps, made shuriken guys glow based on the emission map

--- Assets/Code/Gameplay Systems/InputManager.cs
+++ Assets/Code/Gameplay Systems/InputManager.cs
@@ -7,6 +7,8 @@ public class InputManager : MonoBehaviour
 
     public static InputManager Instance;
 
+    public BaseControls BaseControls { get; private set; }
+
     private void Awake()
     {
         if(Instance == null)
@@ -17,6 +19,9 @@ public class InputManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        BaseControls = new BaseControls();
+        BaseControls.PlayerMap.Enable();
     }
 
     // Update is called once per frame
