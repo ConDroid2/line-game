0fa37980930b251e467b83c7cabe455501ac3307
ConDroid2 <ronnoc918@gmail.com>
2024-08-23 20:12:19 -0500

    Adding sound to unity with wwise

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,150 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_WANDERING = 465142343U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace WANDERINGPAD
+        {
+            static const AkUniqueID GROUP = 2789638893U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID SAWPAD = 3843366911U;
+                static const AkUniqueID SQUAREANDSAWPAD = 309159965U;
+                static const AkUniqueID SQUAREPADWETONLY = 3547219115U;
+                static const AkUniqueID SQUREPADDRYONLY = 1730641605U;
+            } // namespace STATE
+        } // namespace WANDERINGPAD
+
+        namespace WANDERINGPERCUSSION
+        {
+            static const AkUniqueID GROUP = 4286244013U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace WANDERINGPERCUSSION
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace WANDERIGNSQUAREPAD
+        {
+            static const AkUniqueID GROUP = 3053675412U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID OFF = 930712164U;
+                static const AkUniqueID ON = 1651971902U;
+                static const AkUniqueID ONWITH8VADOUBLE = 1066491206U;
+            } // namespace SWITCH
+        } // namespace WANDERIGNSQUAREPAD
+
+        namespace WANDERINGARPEGGIO
+        {
+            static const AkUniqueID GROUP = 2086391272U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID OFF = 930712164U;
+                static const AkUniqueID SAW = 443572616U;
+                static const AkUniqueID SQUAREDRY = 127960571U;
+                static const AkUniqueID SQUAREWITHDELAY = 3696409429U;
+            } // namespace SWITCH
+        } // namespace WANDERINGARPEGGIO
+
+        namespace WANDERINGBASS
+        {
+            static const AkUniqueID GROUP = 2812095919U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ARPINBASS = 2829385960U;
+                static const AkUniqueID DISCOBASS = 114451572U;
+                static const AkUniqueID OFF = 930712164U;
+            } // namespace SWITCH
+        } // namespace WANDERINGBASS
+
+        namespace WANDERINGMELODY
+        {
+            static const AkUniqueID GROUP = 1412217666U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DISCOMELODY = 421134689U;
+                static const AkUniqueID OFF = 930712164U;
+                static const AkUniqueID SAW = 443572616U;
+                static const AkUniqueID SQUARE = 1818333208U;
+            } // namespace SWITCH
+        } // namespace WANDERINGMELODY
+
+        namespace WANDERINGPERCUSSION
+        {
+            static const AkUniqueID GROUP = 4286244013U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID A = 84696446U;
+                static const AkUniqueID B = 84696445U;
+                static const AkUniqueID C = 84696444U;
+                static const AkUniqueID D = 84696443U;
+                static const AkUniqueID OFF = 930712164U;
+            } // namespace SWITCH
+        } // namespace WANDERINGPERCUSSION
+
+        namespace WANDERINGPULSEWAVE
+        {
+            static const AkUniqueID GROUP = 1421364506U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DISCO = 3920460409U;
+                static const AkUniqueID OFF = 930712164U;
+            } // namespace SWITCH
+        } // namespace WANDERINGPULSEWAVE
+
+        namespace WANDERINGSAWPAD
+        {
+            static const AkUniqueID GROUP = 1528543176U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID OFF = 930712164U;
+                static const AkUniqueID ON = 1651971902U;
+            } // namespace SWITCH
+        } // namespace WANDERINGSAWPAD
+
+        namespace ZONE
+        {
+            static const AkUniqueID GROUP = 832057375U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID A = 84696446U;
+                static const AkUniqueID B = 84696445U;
+                static const AkUniqueID C = 84696444U;
+                static const AkUniqueID D = 84696443U;
+                static const AkUniqueID E = 84696442U;
+                static const AkUniqueID F = 84696441U;
+            } // namespace SWITCH
+        } // namespace ZONE
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID TESTSOUNDBANK = 1831431028U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
