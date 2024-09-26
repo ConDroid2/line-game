7f010603ff93bcaecb5d08e66352e1a85b8e3ed6
ConDroid2 <ronnoc918@gmail.com>
2024-09-26 10:06:23 -0500

    Swapping to blue turrets in shootablock, Conveyor lines now have multiple visuals to indicate direction (can be set by frequency variable on component)

--- Assets/Code/LevelComponents/Lines/ConveyorLine.cs
+++ Assets/Code/LevelComponents/Lines/ConveyorLine.cs
@@ -7,14 +7,30 @@ public class ConveyorLine : MonoBehaviour
     [SerializeField] private LineController _lineController;
     [SerializeField] private float _magnitude;
     [SerializeField] [Range(-1, 1)] private int _direction;
+    [SerializeField] private int _visualsFrequency;
 
-    [SerializeField] private Transform _directionVisuals;
+    [SerializeField] private GameObject _directionVisualsPrefab;
+
+    private List<Transform> _directionVisuals;
+    
 
     private void Start()
     {
         //_lineController = GetComponent<LineController>();
 
-        _directionVisuals.up = _lineController.Slope * _direction;
+        int numberOfVisuals = (int)Mathf.Floor(_lineController.Length / _visualsFrequency);
+
+        float distanceBetweenVisuals = _lineController.Length / numberOfVisuals;
+
+        for(int i = 0; i < numberOfVisuals; i++)
+        {
+            GameObject newVisual = Instantiate(_directionVisualsPrefab, transform);
+
+            Vector3 position = _lineController.CurrentA + ( (Vector3)(_lineController.Slope) * (distanceBetweenVisuals * i));
+
+            newVisual.transform.position = position;
+            newVisual.transform.up = _lineController.Slope * _direction;
+        }
     }
 
     private void Update()
