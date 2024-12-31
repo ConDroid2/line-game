ede8a4fc48e1bf2d66be645f32f4eb7d0368a4fd
ConDroid2 <ronnoc918@gmail.com>
2024-12-30 21:32:12 -0600

    Refactoring darkness and lightness code

--- Assets/Code/LevelComponents/DarkAndLight/DarknessController.cs
+++ Assets/Code/LevelComponents/DarkAndLight/DarknessController.cs
@@ -4,30 +4,95 @@ using UnityEngine;
 
 public class DarknessController : MonoBehaviour
 {
-    public float Radius = 1;
-    public List<Transform> Locations;
+    // New Darkness Stuff
+    // Settings
+    [SerializeField] private float _darknessKillTime = 0f;
 
-    public SpriteRenderer Renderer;
-    private Material _darknessMaterial;
 
-    private void Awake()
+    private SafeZone[] _safeZones = { };
+    private Collider2D _playerCollider = null;
+
+    // Player stuff
+    private bool _playerInDarkness = false;
+    private float _timeSincePlayerEnteredDarkness = 0f;
+
+
+    private void Start()
     {
-        _darknessMaterial = Renderer.material;
+        Debug.Log(Player.Instance == null);
+        _safeZones = FindObjectsOfType<SafeZone>();
+
+        if (Player.Instance != null)
+        {
+            _playerCollider = Player.Instance.GetComponent<Collider2D>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        _darknessMaterial.SetFloat("_CircleRadius", Radius);
-
-        List<Vector4> positions = new List<Vector4>();
-
-        for (int i = 0; i <Locations.Count; i++)
+        if (_playerInDarkness)
         {
-            positions.Add(Locations[i].position);
+            if(_timeSincePlayerEnteredDarkness >= _darknessKillTime)
+            {
+                Player.Instance.GetKilled(Enums.KillType.Default);
+            }
+            else
+            {
+                _timeSincePlayerEnteredDarkness += Time.deltaTime;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Check if any part of the player is not outside a light zone
+        // If that check is true, start countdown to player deathk
+
+        bool isPlayerOutsideSafeZoneThisFrame = CheckIfPlayerIsOutsideSafeZone();
+
+        if(isPlayerOutsideSafeZoneThisFrame && _playerInDarkness == false)
+        {
+            _playerInDarkness = true;
+        }
+        else if(isPlayerOutsideSafeZoneThisFrame == false && _playerInDarkness)
+        {
+            _playerInDarkness = false;
+            _timeSincePlayerEnteredDarkness = 0f;
+        }
+    }
+
+    private bool CheckIfPlayerIsOutsideSafeZone()
+    {
+        if(_playerCollider == null)
+        {
+            _playerCollider = Player.Instance.GetComponent<Collider2D>();
         }
 
-        _darknessMaterial.SetVectorArray("_CirclePositions", positions);
+        List<Vector3> playerCorners = new List<Vector3>();
+        playerCorners.Add(new Vector3(_playerCollider.bounds.min.x, _playerCollider.bounds.max.y, 0f));
+        playerCorners.Add(new Vector3(_playerCollider.bounds.max.x, _playerCollider.bounds.max.y, 0f));
+        playerCorners.Add(new Vector3(_playerCollider.bounds.max.x, _playerCollider.bounds.min.y, 0f));
+        playerCorners.Add(new Vector3(_playerCollider.bounds.min.x, _playerCollider.bounds.min.y, 0f));
+
+        foreach(Vector3 corner in playerCorners)
+        {
+            bool cornerInSafeZone = false;
+
+            foreach(SafeZone safeZone in _safeZones)
+            {
+                cornerInSafeZone |= safeZone.Collider.OverlapPoint(corner);
+            }
+
+            if (cornerInSafeZone == false) return true;
+
+            
+        }
+
+        Debug.Log("All corners in safeZone");
+
+        return false;
     }
+
+    
 }
--- Assets/Code/LevelComponents/DarkAndLight/SafeZone.cs
+++ Assets/Code/LevelComponents/DarkAndLight/SafeZone.cs
@@ -4,113 +4,113 @@ using UnityEngine;
 
 public class SafeZone : MonoBehaviour
 {
-    private CircleCollider2D _collider;
+    public CircleCollider2D Collider;
 
     public List<Collider2D> AffectedDarknesses = new List<Collider2D>();
 
     private void Awake()
     {
-        _collider = GetComponent<CircleCollider2D>();
+        Collider = GetComponent<CircleCollider2D>();
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if(collision.gameObject.GetComponent<Player>() != null)
-        {
-            foreach(Collider2D darkness in AffectedDarknesses)
-            {
-                Physics2D.queriesHitTriggers = true;
-                List<Collider2D> hits = new List<Collider2D>(Physics2D.OverlapCircleAll(_collider.bounds.center, _collider.bounds.extents.x));
-                Physics2D.queriesHitTriggers = false;
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if(collision.gameObject.GetComponent<Player>() != null)
+    //    {
+    //        foreach(Collider2D darkness in AffectedDarknesses)
+    //        {
+    //            Physics2D.queriesHitTriggers = true;
+    //            List<Collider2D> hits = new List<Collider2D>(Physics2D.OverlapCircleAll(Collider.bounds.center, Collider.bounds.extents.x));
+    //            Physics2D.queriesHitTriggers = false;
 
-                bool darknessAffectedByOtherSafeZone = false;
+    //            bool darknessAffectedByOtherSafeZone = false;
 
-                foreach(Collider2D hit in hits)
-                {
-                    if (hit.CompareTag("SafeZone"))
-                    {
-                        SafeZone otherSafeZone = hit.GetComponent<SafeZone>();
+    //            foreach(Collider2D hit in hits)
+    //            {
+    //                if (hit.CompareTag("SafeZone"))
+    //                {
+    //                    SafeZone otherSafeZone = hit.GetComponent<SafeZone>();
 
-                        //Debug.Log(otherSafeZone.AffectedDarknesses.Count);
+    //                    //Debug.Log(otherSafeZone.AffectedDarknesses.Count);
 
-                        if(otherSafeZone.AffectedDarknesses.Contains(darkness) == true)
-                        {
-                            darknessAffectedByOtherSafeZone |= true;
-                        }
-                    }
-                }    
+    //                    if(otherSafeZone.AffectedDarknesses.Contains(darkness) == true)
+    //                    {
+    //                        darknessAffectedByOtherSafeZone |= true;
+    //                    }
+    //                }
+    //            }    
                 
-                if(!darknessAffectedByOtherSafeZone)
-                {
-                    darkness.GetComponent<KillOnTouch>().Active = true;
-                }
-            }
-            AffectedDarknesses = new List<Collider2D>();
-        }
-    }
+    //            if(!darknessAffectedByOtherSafeZone)
+    //            {
+    //                darkness.GetComponent<KillOnTouch>().Active = true;
+    //            }
+    //        }
+    //        AffectedDarknesses = new List<Collider2D>();
+    //    }
+    //}
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.GetComponent<Player>() == null) return;
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.GetComponent<Player>() == null) return;
 
-        foreach(Collider2D collider in AffectedDarknesses)
-        {
-            collider.GetComponent<KillOnTouch>().Active = true;
-        }
+    //    foreach(Collider2D collider in AffectedDarknesses)
+    //    {
+    //        collider.GetComponent<KillOnTouch>().Active = true;
+    //    }
 
-        List<Vector3> playerCorners = new List<Vector3>();
-        playerCorners.Add(new Vector3(collision.bounds.min.x, collision.bounds.max.y, 0f));
-        playerCorners.Add(new Vector3(collision.bounds.max.x, collision.bounds.max.y, 0f));
-        playerCorners.Add(new Vector3(collision.bounds.max.x, collision.bounds.min.y, 0f));
-        playerCorners.Add(new Vector3(collision.bounds.min.x, collision.bounds.min.y, 0f));
+    //    List<Vector3> playerCorners = new List<Vector3>();
+    //    playerCorners.Add(new Vector3(collision.bounds.min.x, collision.bounds.max.y, 0f));
+    //    playerCorners.Add(new Vector3(collision.bounds.max.x, collision.bounds.max.y, 0f));
+    //    playerCorners.Add(new Vector3(collision.bounds.max.x, collision.bounds.min.y, 0f));
+    //    playerCorners.Add(new Vector3(collision.bounds.min.x, collision.bounds.min.y, 0f));
 
-        Physics2D.queriesHitTriggers = true;
-        List<Collider2D> hits = new List<Collider2D>(Physics2D.OverlapCircleAll(_collider.bounds.center, _collider.bounds.extents.x));
-        Physics2D.queriesHitTriggers = false;
+    //    Physics2D.queriesHitTriggers = true;
+    //    List<Collider2D> hits = new List<Collider2D>(Physics2D.OverlapCircleAll(Collider.bounds.center, Collider.bounds.extents.x));
+    //    Physics2D.queriesHitTriggers = false;
 
-        AffectedDarknesses = new List<Collider2D>();
-        List<Collider2D> overlappingSafeZones = new List<Collider2D>();
+    //    AffectedDarknesses = new List<Collider2D>();
+    //    List<Collider2D> overlappingSafeZones = new List<Collider2D>();
 
-        foreach (Collider2D hit in hits)
-        {
-            if (hit.CompareTag("Darkness"))
-            {
-                hit.GetComponent<KillOnTouch>().Active = false;
-                AffectedDarknesses.Add(hit);
-                //Debug.Log($"{gameObject.name} is affecting the dark zone");
-            }
-            else if (hit.CompareTag("SafeZone") && hit.gameObject != gameObject)
-            {
-                overlappingSafeZones.Add(hit);
-            }
-        }
+    //    foreach (Collider2D hit in hits)
+    //    {
+    //        if (hit.CompareTag("Darkness"))
+    //        {
+    //            hit.GetComponent<KillOnTouch>().Active = false;
+    //            AffectedDarknesses.Add(hit);
+    //            //Debug.Log($"{gameObject.name} is affecting the dark zone");
+    //        }
+    //        else if (hit.CompareTag("SafeZone") && hit.gameObject != gameObject)
+    //        {
+    //            overlappingSafeZones.Add(hit);
+    //        }
+    //    }
 
 
-        // Check if player should die
-        foreach(Collider2D darkness in AffectedDarknesses)
-        {
-            // If player corner is in darkness and not in this safe zone or any other safe zone
+    //    // Check if player should die
+    //    foreach(Collider2D darkness in AffectedDarknesses)
+    //    {
+    //        // If player corner is in darkness and not in this safe zone or any other safe zone
 
-            foreach(Vector3 corner in playerCorners)
-            {
-                bool cornerInDarkness = darkness.OverlapPoint(corner);
-                bool cornerInThisSafeZone = _collider.OverlapPoint(corner);
+    //        foreach(Vector3 corner in playerCorners)
+    //        {
+    //            bool cornerInDarkness = darkness.OverlapPoint(corner);
+    //            bool cornerInThisSafeZone = Collider.OverlapPoint(corner);
 
-                bool cornerInAnotherSafeZone = false;
+    //            bool cornerInAnotherSafeZone = false;
 
-                foreach(Collider2D safeZone in overlappingSafeZones)
-                {
-                    cornerInAnotherSafeZone |= safeZone.OverlapPoint(corner);
-                }
+    //            foreach(Collider2D safeZone in overlappingSafeZones)
+    //            {
+    //                cornerInAnotherSafeZone |= safeZone.OverlapPoint(corner);
+    //            }
 
-                if(cornerInDarkness == true && cornerInThisSafeZone == false && cornerInAnotherSafeZone == false)
-                {
-                    Player.Instance.GetKilled(Enums.KillType.Darkness);
-                    //Debug.Log(gameObject.name);
-                }
-            }
-        }
+    //            if(cornerInDarkness == true && cornerInThisSafeZone == false && cornerInAnotherSafeZone == false)
+    //            {
+    //                Player.Instance.GetKilled(Enums.KillType.Darkness);
+    //                //Debug.Log(gameObject.name);
+    //            }
+    //        }
+    //    }
 
-        //Debug.Log($"Number of overlapping safe zones: {overlappingSafeZones.Count}");
-    }
+    //    //Debug.Log($"Number of overlapping safe zones: {overlappingSafeZones.Count}");
+    //}
 }
