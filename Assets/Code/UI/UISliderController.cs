497b92927486c5a345e8d415bc78e54db9bb21a5
ConDroid2 <ronnoc918@gmail.com>
2025-01-02 21:22:11 -0600

    Keyholes don't play their animation again. Added a test for code-e pushing block for ui slider

---
+++ Assets/Code/UI/UISliderController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UISliderController : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    public Slider _slider;
+    public Image _codeBody;
+
+    // Tracking data
+    private float _previousValue = 0f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _codeBody.gameObject.SetActive(false);
+
+        _previousValue = _slider.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _slider.Select();
+        }
+    }
+
+    public void HandleValueChange(float newValue)
+    {
+        if(newValue < _previousValue)
+        {
+            _codeBody.rectTransform.anchoredPosition = new Vector2(60f, 0f);
+            _codeBody.rectTransform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            _codeBody.rectTransform.anchoredPosition = new Vector2(-60f, 0f);
+            _codeBody.rectTransform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        _previousValue = newValue;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        _codeBody.gameObject.SetActive(false);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        _codeBody.gameObject.SetActive(true);
+    }
+
+    
+}
