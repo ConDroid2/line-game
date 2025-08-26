647316155f3d21098ca58ef1e3fd8af125f74af5
ConDroid2 <ronnoc918@gmail.com>
2025-08-26 18:22:27 -0500

    Activator lines update

--- Assets/Code/LevelComponents/ActivationIndicatorLine.cs
+++ Assets/Code/LevelComponents/ActivationIndicatorLine.cs
@@ -8,12 +8,32 @@ public class ActivationIndicatorLine : MonoBehaviour
     public Color NotActivatedColor;
     public Color ActivatedColor;
 
+    [SerializeField] Shapes.Polyline _innerLineShape;
+    [SerializeField] Shapes.Polyline _outerLineShape;
+
     [SerializeField] private bool _startActivated = false;
     private bool _activated = false;
 
     private void Awake()
     {
         SetActivated(_startActivated);
+
+        List<Shapes.PolylinePoint> points = new List<Shapes.PolylinePoint>();
+
+        for(int i = 0; i < LineRenderer.positionCount; i++)
+        {
+            Shapes.PolylinePoint polylinePoint;
+
+            polylinePoint = new Shapes.PolylinePoint(transform.InverseTransformPoint(LineRenderer.GetPosition(i)));
+            points.Add(polylinePoint);
+        }
+
+        _innerLineShape.points = points;
+        _innerLineShape.meshOutOfDate = true;
+        _outerLineShape.points = points;
+        _outerLineShape.meshOutOfDate = true;
+
+        LineRenderer.enabled = false;
     }
 
     public void SetActivated(bool activated)
@@ -26,11 +46,15 @@ public class ActivationIndicatorLine : MonoBehaviour
         {
             LineRenderer.startColor = ActivatedColor;
             LineRenderer.endColor = ActivatedColor;
+
+            _innerLineShape.Color = ActivatedColor;
         }
         else
         {
             LineRenderer.startColor = NotActivatedColor;
             LineRenderer.endColor = NotActivatedColor;
+
+            _innerLineShape.Color = NotActivatedColor;
         }
     }
 }
