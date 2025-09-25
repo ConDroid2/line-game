4196120de1f462d407b619b3f362105f369fc350
JPFRACTALTOWER\johnp <john.paul.proper@gmail.com>
2025-09-25 07:40:42 -0500

    Created a fake blue danger zone out of the shapes tool, and then created a script to modulate the alpha level of a shapes-tool shape alternatingly up and down. I did this so that I could change the opacity of a shape with higher emissivity that is otherwise identical to the fake danger zone parts but layered above them, which essentially looks like a increase or decrease in intensity, but without adjusting intensity directly.
    I could not find a way to edit emission directly of shape-tool shape colors.

--- Assets/ColorAlphaModulator.cs
+++ Assets/ColorAlphaModulator.cs
@@ -12,6 +12,7 @@ public class ColorAlphaModulator : MonoBehaviour
 
     public bool firstCallIsIncrease;
     private bool trueIncreaseFalseDecrease;
+    private bool modulationInProgress = false;
 
     private int currentCallNumber = 0; // number to hold the current call
     public int callsToIgnore; // this object will ignore this many calls to events to change the intensity.
@@ -23,17 +24,8 @@ public class ColorAlphaModulator : MonoBehaviour
     void Start()
     {
         _shape = GetComponent<ShapeRenderer>();
-        trueIncreaseFalseDecrease = firstCallIsIncrease;
-        if (firstCallIsIncrease)
-        {
-            timerAction = IncreaseAlpha;
-        }
-        else
-        {
-            timerAction = ReduceAlpha;
-        }
 
-        if(timerAction is null)
+        if (timerAction is null)
         {
             Debug.Log("Timer Action was null before it was set to timer event");
         }
@@ -43,9 +35,25 @@ public class ColorAlphaModulator : MonoBehaviour
             Debug.Log("Timer is null");
         }
 
-        timer.OnPercentDoneChange.AddListener(timerAction); // add this dynamic action to the timer
+        //timer.OnPercentDoneChange.AddListener(timerAction); // add this dynamic action to the timer
         timer.OnTimerEnd.AddListener(ChangeIncreaseDecreaseAction); // add this action to the timer on completion which changes the action done onPercent Change.
         timer.OnTimerEnd.AddListener(() => Debug.Log("Timer completed\n"));
+        timer.OnTimerEnd.AddListener(() => this.modulationInProgress = false); ;
+
+        trueIncreaseFalseDecrease = firstCallIsIncrease;
+        if (firstCallIsIncrease)
+        {
+            //timerAction = IncreaseAlpha;
+            timer.OnPercentDoneChange.AddListener(IncreaseAlpha); // add this dynamic action to the timer
+            this.SetIntensity(minAlpha);
+        }
+        else
+        {
+            //timerAction = ReduceAlpha;
+            timer.OnPercentDoneChange.AddListener(ReduceAlpha); // add this dynamic action to the timer
+            this.SetIntensity(maxAlpha);
+        }
+
     }
 
     //// Update is called once per frame
@@ -83,22 +91,19 @@ public class ColorAlphaModulator : MonoBehaviour
     {
         Debug.Log("ModulateAlpha invoked\n");
         Debug.Log($"CurrentCallNumber = {this.currentCallNumber}\n");
-        Debug.Log($"NewCallNumber = {this.currentCallNumber+1}\n");
-
-        int newCallNumber = this.currentCallNumber + 1 > this.callsToIgnore ? 0 : this.currentCallNumber + 1;
-
-        Debug.Log($"NewCallNumber after potential reset is = {newCallNumber}\n");
-
-        if (newCallNumber == 0) // only do the alpha setting and stuff when the call is set to zero.
+        
+        if (this.currentCallNumber == 0 && !this.modulationInProgress) // only do the alpha setting and stuff when the call is set to zero.
         {
             // When this is called, do the work in the timer, which will call the action set to the OnPercentDoneChange action
             timer.StartTimer();
+            this.modulationInProgress = true;
             Debug.Log($"Timer Started\n");
             // When the timer finishes, it has to swtich the increase/decrease action to use the other function than currently stored
 
         }
 
-        this.currentCallNumber = newCallNumber;
+        this.currentCallNumber = this.currentCallNumber + 1 > this.callsToIgnore ? 0 : this.currentCallNumber + 1;
+        Debug.Log($"NewCallNumber = {this.currentCallNumber}\n");
     }
 
     private void ChangeIncreaseDecreaseAction()
@@ -109,13 +114,17 @@ public class ColorAlphaModulator : MonoBehaviour
         {
             Debug.Log("trueIncreaseFalseDecrease is true\n");
             // therefore set the action to reduce, and set trueIncreaseFalseDecrease to false
-            this.timerAction = this.ReduceAlpha;
+            //this.timerAction = this.ReduceAlpha;
+            timer.OnPercentDoneChange.RemoveListener(IncreaseAlpha);
+            timer.OnPercentDoneChange.AddListener(ReduceAlpha);
         }
         else // trueIncreaseFalseDecrease is false, therefore the last operation that occured was a reduction in intensity.
         {
             Debug.Log("trueIncreaseFalseDecrease is false\n");
             // therefore set the action to increase alpha.
-            this.timerAction = this.IncreaseAlpha;
+            //this.timerAction = this.IncreaseAlpha;
+            timer.OnPercentDoneChange.RemoveListener(ReduceAlpha);
+            timer.OnPercentDoneChange.AddListener(IncreaseAlpha);
         }
 
         // reverse trueIncreaseFalseDecrease so that the action switches when this is called again.
